f0bdc9d24d2008e87dc4bebfbcb85f174c0b8288
igsrodino <igs_rodino@hotmail.com>
2020-06-18 11:07:25 +1000

    Added sample person/user and service

---
+++ src/main/java/com/example/demo/Service/PersonService.java
@@ -0,0 +1,17 @@
+package com.example.demo.Service;
+
+import com.example.demo.Dao.PersonDAO;
+import com.example.demo.Model.Person;
+
+public class PersonService {
+
+    private final PersonDAO personDAO;
+
+    public PersonService(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    public int addPerson(Person person) {
+        return personDAO.insertPerson(person);
+    }
+}
